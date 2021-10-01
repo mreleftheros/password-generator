0db10e9b7e9e9ca8f07045c7083f67d6f44102c0
eleftheros <eleftheros_all@protonmail.com>
2021-10-01 18:00:49 +0300

    Update updateCopyBtn and copyPassword functions

--- src/js/index.js
+++ src/js/index.js
@@ -13,19 +13,25 @@ const init = () => {
   ];
 
   // adds active class and updates btn text
-  const updateCopyBtn = () => {
-    copyBtn.classList.add("active");
-    copyBtn.textContent = "Copied!";
+  const updateCopyBtn = (e) => {
+    if (copyBtn.classList.contains("active")) {
+      if (e.type === "click") return; // check
+      copyBtn.textContent = "Copy";
+    }
+    else {
+      copyBtn.textContent = "Copied";
+    }
+
+    copyBtn.classList.toggle("active");
   };
 
   // copies current password and calls updateCopyBtn function
   const copyPassword = e => {
-    let passwordElement = e.target.previousElementSibling;
+    if (textarea.value === "") return; // check
 
-    passwordElement.select();
-    document.execCommand("copy");
+    navigator.clipboard.writeText(textarea.value);
 
-    return updateCopyBtn();
+    return updateCopyBtn(e);
   };
 
   // helper function that returns the value of random index of a string
@@ -49,32 +55,43 @@ const init = () => {
     let randomindex = getRandomIndex(len);
 
     while (arr[randomindex]) {
-      randomindex = getRandomIndex(len);
+      randomindex = (randomindex + 1) % len;
     }
 
-    arr.splice(randomindex, 0, item);
+    arr.splice(randomindex, 1, item);
   };
 
+  const showPassword = password => {
+    textarea.value = "";
+    textarea.value = password;
+  };
+
+  // generates random password based on form params
   const generatePassword = (passwordLen, fields) => {
     let password = [];
+    password.length = passwordLen;
     let fieldsLen = fields.length;
+    let remainder;
+    let len;
+    let str;
 
     switch (fieldsLen) {
       case 0:
         return;
         break;
       case 1:
-        let str = params.filter(param => param.name === fields[0])[0].value;
+        str = params.filter(param => param.name === fields[0])[0].value;
 
         for (let i = 0; i < passwordLen; i++) {
           password.push(getRandomValue(str));
         }
         break;
       case 2:
-        let remainder = passwordLen;
+        remainder = passwordLen;
+
         fields.forEach((field, i, arr) => {
-          let str = params.filter(param => param.name === field)[0].value;
-          let len;
+          str = params.filter(param => param.name === field)[0].value;
+
           if (i === arr.length - 1) { // last
             len = remainder;
           } else {
@@ -83,19 +100,68 @@ const init = () => {
           }
 
           for (let i = 0; i < len; i++) {
-            password.push(getRandomValue(str));
+            setItemIntoRandomIndex(getRandomValue(str), password);
           }
         })
+        break;
       case 3:
+        remainder = passwordLen;
+
+        fields.forEach((field, i, arr) => {
+          str = params.filter(param => param.name === field)[0].value;
+
+          if (i === arr.length -1) { // last
+            len = remainder;
+          } 
+          else {
+            do {
+              len = getRandomNumber(remainder);
+            } while (remainder - len < arr.length - (i + 1))
+  
+            remainder -= len;
+          }
+          
+          for (let i = 0; i < len; i++) {
+            setItemIntoRandomIndex(getRandomValue(str), password);
+          }
+        })
+        break;
       case 4:
+        remainder = passwordLen;
+
+        fields.forEach((field, i, arr) => {
+          str = params.filter(param => param.name === field)[0].value;
+
+          if (i === arr.length -1) { // last
+            len = remainder;
+          } 
+          else {
+            do {
+              len = getRandomNumber(remainder);
+            } while (remainder - len < arr.length - (i + 1))
+  
+            remainder -= len;
+          }
+          
+          for (let i = 0; i < len; i++) {
+            setItemIntoRandomIndex(getRandomValue(str), password);
+          }
+        })
+        break;
     }
 
-    console.log(password.join(""), password.length);
+    password = password.join("");
+
+    return showPassword(password);
   };
 
   // submits password form and calls generatePassword with given value fields
   const submitPasswordForm = e => {
     e.preventDefault();
+
+    if (copyBtn.classList.contains("active")) {
+      updateCopyBtn(e);
+    }
 
     let passwordLength = 30;
     let fields = [
@@ -108,8 +174,6 @@ const init = () => {
     fields = fields
       .filter(field => field.value)
       .map(field => field.name);
-
-    e.target.reset();
 
     return generatePassword(passwordLength, fields);
   };
